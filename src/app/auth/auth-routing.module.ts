f24d12ed27c75aa3a8169f1936b8dcaa65b40872
Dipti <singh236dipti@gmail.com>
2025-03-15 11:11:48 +0530

    angular project

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegistrationComponent } from './components/auth/registration/registration.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+
+const routes: Routes = [
+  {
+    path:'login',
+    component:LoginComponent  
+  },
+  {
+    path:'register',
+    component:RegisterComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
